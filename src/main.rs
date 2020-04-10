91a03d7d149ae67b808875ce3fbfeec2e4fb035c
Mirko Covizzi <mrkcvzz@gmail.com>
2020-04-10 23:10:12 +0200

    Add Graph: Number of Islands

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod buy_sell_stock;
 mod sum_two_integers;
 mod climbing_stairs;
 mod longest_substring_without_repeating_characters;
+mod number_of_islands;
 
 fn main() {
 
---
+++ src/number_of_islands/mod.rs
@@ -0,0 +1,74 @@
+// https://leetcode.com/problems/number-of-islands/
+
+fn dfs(grid: &mut Vec<Vec<char>>, i: i32, j: i32) -> i32 {
+    let i: usize = i as usize;
+    let j: usize = j as usize;
+
+    if i < 0 ||
+        i >= grid.len() ||
+        j < 0 ||
+        j >= grid[i].len() ||
+        grid[i][j] == '0' {
+        return 0;
+    }
+
+    grid[i][j] = '0';
+
+    let i: i32 = i as i32;
+    let j: i32 = j as i32;
+
+    dfs(grid, i - 1, j);
+    dfs(grid, i + 1, j);
+
+    dfs(grid, i, j - 1);
+    dfs(grid, i, j + 1);
+
+    1
+}
+
+pub fn num_islands(grid: &mut Vec<Vec<char>>) -> i32 {
+    if grid.len() == 0 {
+        return 0;
+    }
+
+    let mut num_islands = 0;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j] == '1' {
+                num_islands += dfs(grid, i as i32, j as i32);
+            }
+        }
+    }
+
+    num_islands
+}
+
+#[cfg(test)]
+mod tests {
+    use super::num_islands;
+
+    #[test]
+    fn given_example_1() {
+        let input = vec![
+            vec!['1', '1', '1', '1', '0'],
+            vec!['1', '1', '0', '1', '0'],
+            vec!['1', '1', '0', '0', '0'],
+            vec!['1', '1', '0', '0', '0'],
+        ];
+        let output = 1;
+        assert_eq!(num_islands(&mut input.clone()), output);
+    }
+
+    #[test]
+    fn given_example_2() {
+        let input = vec![
+            vec!['1', '1', '0', '0', '0'],
+            vec!['1', '1', '0', '0', '0'],
+            vec!['0', '0', '1', '0', '0'],
+            vec!['0', '0', '0', '1', '1'],
+        ];
+        let output = 3;
+        assert_eq!(num_islands(&mut input.clone()), output);
+    }
+}
