0a0f8d61dec664500ff09693cfa30613f1ccdd7e
Mirko Covizzi <mrkcvzz@gmail.com>
2020-04-10 17:25:25 +0200

    Change hashmap to hashset

--- src/longest_substring_without_repeating_characters/mod.rs
+++ src/longest_substring_without_repeating_characters/mod.rs
@@ -1,30 +1,30 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 /// First of all we can't do better than O(N) where N is the length of the string
 /// because we have to at least check the whole string.
 /// The idea of this algorithm is to check the string char by char, adding each
-/// non-repeating char to an hashmap so at the next iteration we can see if that
+/// non-repeating char to an hashset so at the next iteration we can see if that
 /// current char is already present in the current substring, whose chars are keys
-/// of the hashmap.
+/// of the hashset.
 /// If it is already present, we check if the substring length improves upon the best
 /// solution found so far and if it does we update it.
-/// Then, we clear the hashmap and add the current char to it.
-/// If it's not present we simply add it to the hashmap.
+/// Then, we clear the hashset and add the current char to it.
+/// If it's not present we simply add it to the hashset.
 /// The return value is the length of the best (longest) substring candidate.
 pub fn length_of_longest_substring(s: String) -> i32 {
-    let mut hm = HashMap::new();
+    let mut hs = HashSet::new();
     let mut best = 0;
 
     for i in s.chars() {
-        if hm.contains_key(&i) {
-            if hm.keys().len() > best {
-                best = hm.keys().len();
+        if hs.contains(&i) {
+            if hs.len() > best {
+                best = hs.len();
             }
-            hm.clear();
+            hs.clear();
         }
-        hm.insert(i, 0);
+        hs.insert(i);
     }
     best as i32
 }
