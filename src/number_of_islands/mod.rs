f9cfa9c19e4f737969757f5224f6eeec823d63db
Mirko Covizzi <mrkcvzz@gmail.com>
2020-04-10 23:12:36 +0200

    Adjust function signature to match leetcode suggested signature

--- src/number_of_islands/mod.rs
+++ src/number_of_islands/mod.rs
@@ -26,7 +26,9 @@ fn dfs(grid: &mut Vec<Vec<char>>, i: i32, j: i32) -> i32 {
     1
 }
 
-pub fn num_islands(grid: &mut Vec<Vec<char>>) -> i32 {
+pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+    let mut grid = grid.clone();
+
     if grid.len() == 0 {
         return 0;
     }
@@ -36,7 +38,7 @@ pub fn num_islands(grid: &mut Vec<Vec<char>>) -> i32 {
     for i in 0..grid.len() {
         for j in 0..grid[i].len() {
             if grid[i][j] == '1' {
-                num_islands += dfs(grid, i as i32, j as i32);
+                num_islands += dfs(&mut grid, i as i32, j as i32);
             }
         }
     }
@@ -57,7 +59,7 @@ mod tests {
             vec!['1', '1', '0', '0', '0'],
         ];
         let output = 1;
-        assert_eq!(num_islands(&mut input.clone()), output);
+        assert_eq!(num_islands(input), output);
     }
 
     #[test]
@@ -69,6 +71,6 @@ mod tests {
             vec!['0', '0', '0', '1', '1'],
         ];
         let output = 3;
-        assert_eq!(num_islands(&mut input.clone()), output);
+        assert_eq!(num_islands(input), output);
     }
 }
